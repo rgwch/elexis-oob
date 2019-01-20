880ca74fcfede0f1fef1e1d30e885e317e5826c1
rgwch <weirich@elexis.ch>
2019-01-20 17:56:25 +0100

    diverses

--- appserver/site/routes/index.js
+++ appserver/site/routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+const ip=require('ip').address()
+
+var net = require('net');
+
+function getNetworkIP(callback) {
+  var socket = net.createConnection(80, 'www.google.com');
+  socket.on('connect', function() {
+    callback(undefined, socket.address().address);
+    socket.end();
+  });
+  socket.on('error', function(e) {
+    callback(e, 'error');
+  });
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Elexis Out-Of-The-Box' });
+  
+  res.render('index', { title: 'Elexis Out-Of-The-Box',
+                         ip: ip });
 });
 
 module.exports = router;
