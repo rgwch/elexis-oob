9629ca0acd49b0703248cfbd8d67d6fdbb858996
gerry <weirich@elexis.ch>
2019-02-02 07:58:15 +0100

    backup file

---
+++ appserver/backup.sh
@@ -0,0 +1,36 @@
+#! /bin/sh
+
+fdate=`date '+%Y-%m-%d-%H%M'`
+dest=/backup
+dirname=$dest'/elexisbackup_'$fdate
+log=/backup/lastlog.log
+
+echo $dirname > $log
+mkdirs $dirname
+rc=$?; if [[ $rc != 0 ]]; then
+	echo could not create $dirname: $rc >>$log
+	exit $rc
+fi
+log=$dirname/backup.log
+echo start `date` > $log
+
+make_backup(){
+  name=$1
+  file=$dirname/$name.tgz
+  echo $file `date` >> $log
+  tar -czf $file /mnt/$name
+  rc=$?; if [[ $rc != 0 ]]; then
+	  echo $name failed with error code $rc >>$log
+	  exit $rc
+  fi
+
+}
+
+make_backup elexisdb
+make_backup lucindadata
+make_backup lucindabase
+make_backup webelexisdata
+make_backup pacsdata
+
+
+echo elexisbackup ended normally >>$log
