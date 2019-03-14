280958bd5c3b8c2d93b3194210a9a3d0b8a8cf0e
rgwch <rgw@elexis.ch>
2019-03-14 19:38:39 +0100

    div

--- appserver/site/tests/backup.spec.js
+++ appserver/site/tests/backup.spec.js
@@ -2,10 +2,12 @@ const chai=require('chai')
 chai.should()
 const fs=require('fs')
 const path=require('path')
+const rimraf=require('rimraf')
+const archiver=require('../utils/archiver')
 
 const testdir=path.join(__dirname,"output")
 before(done=>{
-    fs.rmdir(testdir,err=>{
+    rimraf(testdir,err=>{
         if(err){
             console.log(err)
         }
@@ -18,6 +20,9 @@ before(done=>{
 
 describe("Backup",()=>{
     it("creates a backup of this directory",async ()=>{
-
+        const tosave=path.join(__dirname,"../public/doc")
+        const arc=new archiver(testdir,3)
+        const result=await arc.pack(tosave)
+        console.log(result)
     })
 })
